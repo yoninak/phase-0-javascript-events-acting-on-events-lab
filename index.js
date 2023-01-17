592499cbba982cbdc6adf76b0f0f9dbd12ec1d8a
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-01-17 12:14:19 -0500

    commit tree da32b2ba92db093711ffe1460f26980ad4b50014

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+undefined
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+undefined
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+undefined
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
